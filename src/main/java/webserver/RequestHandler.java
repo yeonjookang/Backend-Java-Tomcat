367e378594ebe751050944c49ab288079f66017e
kang yeon joo <rkdduswndi@naver.com>
2023-09-30 19:44:34 +0900

    feat: 기능 요구사항 1-1. index.html 반환 해결

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,10 +2,17 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RequestHandler implements Runnable{
+
+    /**
+     * 연결 소켓을 전달받아 클라이언트의 요청을 처리하고 응답을 생성하는 클래스
+     */
+
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
@@ -17,12 +24,51 @@ public class RequestHandler implements Runnable{
     public void run() {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            /**
+             * connection 객체를 이용해서 클라이언트로부터의 입력 스트림을 얻어와 요청 데이터를 읽을 수 있다.
+             * 또한 출력 스트림을 얻어와 HTTP 응답을 클라이언트에게 보낼 수 있다.
+             */
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in)); //줄 단위로 읽어서 요청 헤더 및 바디를 쉽게 처리
+            DataOutputStream dos = new DataOutputStream(out); //바이트 데이터를 클라이언트에게 보내는 데에 사용
+
+            /**
+             * br의 첫번째 줄은 다음과 같은 정보가 들어있다.
+             * HTTP_METHOD URL HTTP_VERSION
+             */
+            String requestLine = br.readLine(); // 첫 번째 줄을 읽음
+            String[] requestParts = requestLine.split(" "); // 공백으로 분리하여 HTTP 메서드, URL, HTTP 버전을 얻음
+
+            //byte[] body = "Hello World".getBytes();
+
+            if (requestParts.length >= 2) { //올바르지 않은 HTTP 형식 예외 처리
+                String httpMethod = requestParts[0];
+                String url = requestParts[1];
+
+                // url에는 요청한 자원의 경로와 쿼리 문자열이 포함됨
+                System.out.println("HTTP Method: " + httpMethod);
+                System.out.println("URL: " + url);
+
+                byte[] body;
+
+                // String은 객체이므로 == 비교 연산은 주소 비교가 이루어짐!
+                if(url.equals("/") || url.equals("/index.html")){
+
+                    /**
+                     * Files 라이브러리를 이용해서 파일의 정보를 바이트 단위로 변환
+                     */
+
+                    body = Files.readAllBytes(Paths.get("C:\\Users\\rkddu\\KUIT2_Server_Mission_3\\Backend-Java-Tomcat\\webapp\\index.html"));
+                    response200Header(dos, body.length); // 응답의 헤더를 생성
+                    responseBody(dos, body); //응답의 본문을 생성
+                }else{
+                    body = "".getBytes();
+                }
+                response200Header(dos, body.length); // 응답의 헤더를 생성
+                responseBody(dos, body); //응답의 본문을 생성
+            }
+
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -9,7 +9,20 @@ import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public class WebServer {
-    private static final int DEFAULT_PORT = 80;
+
+    /**
+     * HTTP 요청을 받고, 응답을 보내는 역할의 웹 서버
+     */
+
+    private static final int DEFAULT_PORT = 80; // 포트 번호 80으로 지정
+
+    /**
+     * FIFO 구조를 멀티 스레딩 구현함으로서 먼저 해결된 요청 먼저 전달하도록 변경
+     * 1. DEFAULT_THREAD_NUM 수의 스레드를 가진 스레드 풀 생성
+     * 2. 나머지 작업은 큐에서 대기함
+     * 3. 과도한 스레드 생성 및 관리로 인한 성능 문제 방지
+     */
+
     private static final int DEFAULT_THREAD_NUM = 50;
     private static final Logger log = Logger.getLogger(WebServer.class.getName());
 
@@ -18,16 +31,25 @@ public class WebServer {
         ExecutorService service = Executors.newFixedThreadPool(DEFAULT_THREAD_NUM);
 
         if (args.length != 0) {
+            //커맨드 라인에서 입력받은 포트 번호를 사용하거나 기본 포트 번호를 사용
             port = Integer.parseInt(args[0]);
         }
 
-        // TCP 환영 소켓
+        /**
+         * 요청에 대한 처리를 모두 하나의 포트에서 처리하면 과부화
+         * 따라서 환영 포트(welcomeSocket)는 80으로 하되, 연결 소켓(connection)은 매번 다른 포트 번호로 넘긴다.
+         */
+
+        // TCP 환영 소켓 : 클라이언트의 연결을 수락하기 위한 환영 소켓
         try (ServerSocket welcomeSocket = new ServerSocket(port)){
 
-            // 연결 소켓
+            // 연결 소켓 : 실제 연결된 클라이언트와의 통신을 담당
             Socket connection;
             while ((connection = welcomeSocket.accept()) != null) {
-                // 스레드에 작업 전달
+                /**
+                 * 스레드에 Socket을 전달 -> 다수의 클라이언트 요청을 병렬로 처리 가능
+                 * RequestHandler 클래스가 클라이언트와의 통신 및 HTTP 요청 처리 담당
+                 */
                 service.submit(new RequestHandler(connection));
             }
         }
